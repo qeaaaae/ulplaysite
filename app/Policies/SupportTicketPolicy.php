9b2ae4acb8ba384fc167a3638ecdbdd3c36e1954
Rafael <satkevich_rafael@proviante.ru>
2026-03-23 13:12:48 +0400

    23.03 upd

--- app/Policies/SupportTicketPolicy.php
+++ app/Policies/SupportTicketPolicy.php
@@ -11,6 +11,6 @@ class SupportTicketPolicy
 {
     public function view(User $user, SupportTicket $ticket): bool
     {
-        return $ticket->user_id === $user->id;
+        return $user->is_admin || $ticket->user_id === $user->id;
     }
 }
